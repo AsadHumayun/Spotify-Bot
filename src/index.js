6c5e54d26140d127576346abee67984d291ebf98
Asad <asad@asadh.dev>
2023-09-15 17:30:19 +0100

    uninstall qs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import { stringify } from 'querystring';
 import { config } from 'dotenv';
 import { initDb } from './db/init.js';
 import './bot/main.js';
