128c8ef7e76249d3e25266379c0ae3c57eeb674d
Asad <asad@asadh.dev>
2023-11-07 16:35:49 +0000

    Update register.js

--- src/bot/commands/register.js
+++ src/bot/commands/register.js
@@ -13,6 +13,6 @@ export default {
 				return message.channel.send(`An exception occurred :3\n\`\`\`${e}\n${e.stack}\`\`\``);
 			});
 
-		message.channel.send(`Successfully created object with Pk<${args[0] || message.channel.id}> postAccount<${args[1] || message.author.id}>`);
+		message.channel.send(`Successfully bound #${args[0] || message.channel.id} to U:<${args[1] || message.author.id}>`);
 	},
 };
